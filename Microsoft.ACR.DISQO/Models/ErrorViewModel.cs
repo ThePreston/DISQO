5a0da98ed662fbe00d66e5aa77f1cfb5c27ecb1f
Preston Hale <prestonhale@microsoft.com>
2021-08-13 14:09:36 -0500

    Add project files.

---
+++ Microsoft.ACR.DISQO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Microsoft.ACR.DISQO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
